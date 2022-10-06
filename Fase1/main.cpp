33427cd90d590f118ecfc0b06328732bae3eddb7
RenattoGuzman <renttogs@gmail.com>
2022-10-06 08:01:01 -0600

    creacion de estructuras

--- Fase1/main.cpp
+++ Fase1/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <pthread.h>
+
+using namespace std;
+
+struct Tienda {
+    int id;
+    int distancia;
+    int pedidos[4];
+};
+
+struct Camion {
+    int id;
+    string tipoCarga;
+    Tienda ruta[5];
+    long carga;
+    int TiempoCarga;
+    int TiempoRuta;
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Simulacion de Entrega" << endl;
+
+
+
+
+
+
+
+
+
     return 0;
 }
