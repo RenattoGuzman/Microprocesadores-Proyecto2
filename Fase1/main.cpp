a23c5bea759b78bfad7e32a9774f1bb9a3eb519f
adrianfulla <ful21592@uvg.edu.gt>
2022-10-09 16:41:30 -0600

    Creacion de Tiendas
    Se completo el metodo para crear tiendas y pedidos a camiones

--- Fase1/main.cpp
+++ Fase1/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <pthread.h>
+#include <random>
 
 using namespace std;
 
 struct Tienda {
     int id;
     int distancia;
-    int pedidosTienda[4];
+    vector<int> pedidosTienda;
 };
 
 struct Camion {
@@ -18,33 +19,82 @@ struct Camion {
     int TiempoRuta;
 };
 
-void creacionTienda(Tienda t){
+int KmH;
+int Tcarga;
+int Tdescarga;
+int hId=0;
+Tienda T[5];
+Camion C[4];
+
+
+Tienda creacionTienda(int id){
+    cout << "Creacion Tienda"<< endl;
+
+    random_device rd;
+    mt19937 mt(rd());
+    uniform_real_distribution<double> dist(0.0,10.0);
+
+    vector<int> arr;
+
+    for (int i =0; i< 4;i++){
+        int num =(int) (dist(mt)*10);
+        if(num <= 25)
+            arr.push_back(0);
+        else
+            arr.push_back(num);
+    }
+    Tienda t = {id+1,25*(id+1),arr};
+    return t;
 
 }
 
-
 void *asignacionTienda(void * args){
+    int id = hId++;
+    cout << "Asignacion"<< endl;
 
+    T[id]= creacionTienda(id);
 }
 
 void *CargaDescarga(void * args){
 
+    cout << "Carga Descarga"<< endl;
+
 }
 
 void *RutaCamion(void * args){
+
+    cout << "Ruta"<< endl;
 
 }
 
 
 
 int main() {
-    Tienda T[5];
-    Camion C[4];
-
-
 
 
     cout << "Simulacion de Entrega" << endl;
+
+    //cout << "\nIngrese la velocidad promedio de los camiones: " << endl;
+    //cin >> KmH;
+    //cout << "\nIngrese la velodidad de carga: " << endl;
+    //cin >> Tcarga;
+    //cout << "\nIngrese la velocidad de descarga: " << endl;
+    // cin >> Tdescarga;
+
+
+    pthread_t hilo;
+
+    pthread_create(&hilo, NULL,asignacionTienda,NULL);
+    //pthread_create(&hilo, NULL,CargaDescarga,&C[0]);
+    //pthread_create(&hilo, NULL,RutaCamion,&C[0]);
+
+    pthread_join(hilo,NULL);
+
+    cout<<"Id: "<< T[0].id<<endl;
+    cout<<"Distancia: "<< T[0].distancia<<endl;
+    cout<<"Pedidos: "<<endl;
+    for (int i = 0; i<4;i++)
+        cout <<"Camion " <<i <<" pedido: "<< T[0].pedidosTienda[i]<<endl;
 
 
 
@@ -56,3 +106,4 @@ int main() {
 
     return 0;
 }
+
